b7f180b8cefc18fcb3c9c53f5cbecdddb634232e
nevo <nevo@live.com>
2019-03-03 12:30:31 +0200

    multiple contacts and JSONs

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import json
 from whatsapp.whatsapp import WhatsApp
 
 if __name__ == '__main__':
-    wa = WhatsApp(contact_name="Shaked", hours_between_msg=0.01)
-    wa.run()
+    wa = WhatsApp()
+
+    json_file = open(r"messages/whatsapp_friend_1.json", encoding='utf8')
+    messages = json.load(json_file)
+    wa.run(contact_name="Guy Danielli CDALab", hours_between_msg=0.01, number_of_msg=15, messages=messages)
+
+    json_file = open(r"messages/whatsapp_friend_2.json", encoding='utf8')
+    messages = json.load(json_file)
+    wa.run(contact_name="Notes", hours_between_msg=0.01, number_of_msg=3, messages=messages)
 
     print("Goodbye :)")
--- whatsapp/whatsapp.py
+++ whatsapp/whatsapp.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import configparser
 import time
-import json
 from datetime import datetime
 from datetime import time as clock
 import random
@@ -14,11 +13,8 @@ setting = config['SETTINGS']
 # Setting up variable from config file
 driver_path = setting['WEB_DRIVER']
 website_url = setting['WEBSITE_URL']
-msg_json = setting['JSON']
-json_file = open(msg_json, encoding='utf8')
-messages = json.load(json_file)
 time_to_sleep = int(setting['TIME_TO_SLEEP'])
-range_default = int(setting['RANGE'])
+range_default = int(setting['NUMBER_OF_MSG'])
 time_night_start = int(setting['TIME_NIGHT_START'])
 time_night_end = int(setting['TIME_NIGHT_END'])
 time_morning_start = int(setting['TIME_MORNING_START'])
@@ -28,32 +24,33 @@ time_hour_start = int(setting['START_HOUR'])
 
 class WhatsApp(object):
 
-    def __init__(self, contact_name: str, hours_between_msg: float):
-        self.time_between_msg: int = int(hours_between_msg * 3600)  # hours to seconds
-        self.phone_name = contact_name
+    def __init__(self, ):
         self.browser = webdriver.Chrome(driver_path)
         self.browser.get(website_url)
         print("Please go to your smart phone -> WhatsApp -> WhatsApp Web -> Use the QR code reader to connect"
               "\nNote: you have only {0} seconds to connect".format(time_to_sleep))
         time.sleep(time_to_sleep)  # time to connect
+
+    def run(self, contact_name: str, hours_between_msg: float, number_of_msg: int, messages: dict):
+        time_between_msg: int = int(hours_between_msg * 3600)  # hours to seconds
+        phone_name = contact_name
         # Finding the search element
-        self.document_element = self.browser.find_element_by_xpath("//input[@title='Search or start new chat']")
-        self.document_element.click()
-        self.document_element.send_keys(self.phone_name)
+        document_element = self.browser.find_element_by_xpath("//input[@title='Search or start new chat']")
+        document_element.click()
+        document_element.send_keys(phone_name)
         time.sleep(time_to_sleep)
         # Finding the contact name
-        self.browser.find_element_by_xpath("//*[@title='" + self.phone_name + "']").click()
+        self.browser.find_element_by_xpath("//*[@title='" + phone_name + "']").click()
         time.sleep(time_to_sleep)
         # not sure
-        self.document_element = self.browser.find_element_by_xpath("//div[@data-tab='1']")
-        self.document_element.click()
+        document_element = self.browser.find_element_by_xpath("//div[@data-tab='1']")
+        document_element.click()
 
-    def run(self):
         print("Welcome to AutomaticSocial, we will send your messages to {0}, every {1} hours ({2} seconds),"
-              "\nThank you for using our program!".format(self.phone_name,
-                                                          self.time_between_msg/3600,
-                                                          self.time_between_msg))
-        for i in range(range_default):
+              "\nThank you for using our program!".format(phone_name,
+                                                          time_between_msg/3600,
+                                                          time_between_msg))
+        for i in range(number_of_msg):
             now = datetime.now()
             now_time = now.time()
             if clock(time_night_start, time_hour_start) <= now_time <= clock(time_night_end, time_hour_start):
@@ -63,7 +60,7 @@ class WhatsApp(object):
                 key_in_dict, value_in_dict = random.choice(list(messages['morning'].items()))
                 message_to_send = value_in_dict
             else:
-                key_in_dict, value_in_dict = random.choice(list(messages['day'].items()))
+                key_in_dict, value_in_dict = random.choice(list(messages['noon'].items()))
                 message_to_send = value_in_dict
-            self.document_element.send_keys(message_to_send + " " + Keys.ENTER)
-            time.sleep(self.time_between_msg)
+            document_element.send_keys(message_to_send + " " + Keys.ENTER)
+            time.sleep(time_between_msg)
