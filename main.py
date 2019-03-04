e346bb7d9cd4c50abeb19f7a7ff4d5e6e941d2f5
Shaked Eyal <shaked.eyal@gmail.com>
2019-03-04 19:37:09 +0200

    check facebook

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 from whatsapp.whatsapp import WhatsApp
+from facebook.facebook import Facebook
 
 if __name__ == '__main__':
+    """
     wa = WhatsApp()
 
     json_file = open(r"messages/whatsapp_friend_1.json", encoding='utf8')
@@ -11,5 +13,12 @@ if __name__ == '__main__':
     json_file = open(r"messages/whatsapp_friend_2.json", encoding='utf8')
     messages = json.load(json_file)
     wa.run(contact_name="Notes", hours_between_msg=0.01, number_of_msg=3, messages=messages)
+    """
+
+    f = Facebook('shaked.eyal@protonmail.com')
+    try:
+        f.send_message_friend('Nevo Itzhak', 'Send automatic from python')
+    except Exception:
+        print('Failed to send message')
 
     print("Goodbye :)")
